61dda14bf71fe07441d9167fa3e172f3bd53ee55
duhanhasanain <duhan1325@gmail.com>
2025-03-10 23:02:21 +0700

    “menambahkan implementasi dengan state-based construction”

--- tpmodul4_103022330080/Program.cs
+++ tpmodul4_103022330080/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class Program
+﻿public class Program
 {
     internal class kelurahan
     {
@@ -15,4 +14,60 @@ public class Program
         }
     }
 
+    public enum doorState { TERBUKA, TERKUNCI };
+    public enum doorTrigger { KUNCI_PINTU, BUKA_PINTU };
+
+    class Door
+    {
+        public doorState currentState = doorState.TERKUNCI;
+        public class Transition
+        {
+            public doorState currentState;
+            public doorState finalState;
+            public doorTrigger trigger;
+
+            public Transition(doorState currentState, doorState finalState, doorTrigger trigger)
+            {
+                this.currentState = currentState;
+                this.finalState = finalState;
+                this.trigger = trigger;
+            }
+        }
+        Transition[] transisi =
+        {
+            new Transition(doorState.TERKUNCI, doorState.TERKUNCI, doorTrigger.KUNCI_PINTU),
+            new Transition(doorState.TERBUKA,doorState.TERBUKA, doorTrigger.BUKA_PINTU),
+            new Transition(doorState.TERKUNCI, doorState.TERBUKA, doorTrigger.BUKA_PINTU),
+            new Transition(doorState.TERBUKA, doorState.TERKUNCI, doorTrigger.KUNCI_PINTU)
+
+        };
+
+        private doorState GetNextState(doorState currentState, doorTrigger trigger)
+        {
+            doorState finalState = currentState;
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+                if (currentState == perubahan.currentState && trigger == perubahan.trigger)
+                {
+                    finalState = perubahan.finalState;
+                }
+            }
+            return finalState;
+        }
+
+        public void ActivateTrigger(doorTrigger trigger)
+        {
+            currentState = GetNextState(currentState, trigger);
+            Console.WriteLine("Kondisi pintu sekarang adalah " + currentState);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Door machine = new Door();
+        machine.ActivateTrigger(doorTrigger.BUKA_PINTU);
+        machine.ActivateTrigger(doorTrigger.BUKA_PINTU);
+        machine.ActivateTrigger(doorTrigger.KUNCI_PINTU);
+    }
 }
